c92fdc2a969516531bd2403d80add68aa710b8c8
zhuzhunadzee <zhuzhunadze.1@gmail.com>
2021-01-28 23:05:19 +0400

    render models

--- carvash/washapp/views.py
+++ carvash/washapp/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse, get_object_or_404
+from .models import HireCompany, Location, WasherCompany
 
 # Create your views here.
+def most_common(l):
+    return max(set(l), key=l.count)
+
+def home(request):
+    comp=WasherCompany.objects.all()
+    loc=Location.objects.all()
+    li=[i.city for i in loc]
+    common_city=most_common(li)
+    
+    return render(request, "washapp/home.html", {"company":comp, "location":loc, "common":common_city})
+
+def company_order(request, pk):
+    order=HireCompany.objects.get(company=pk)
+    return render(request, "washapp/detail.html", {"order":order})
+    
